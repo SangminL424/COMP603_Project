478d738ffcd401e0b3d03d34deb9a4db2d3e226b
SangminL424 <zwty21@gmail.com>
2024-10-03 15:24:08 +1300

    add quit function to quitButton

--- Comp603Assignment2/src/Controller.java
+++ Comp603Assignment2/src/Controller.java
@@ -1,17 +1,57 @@
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Controller {
+
     View view = new View();
-    
-    public Controller(View view){
+
+    public Controller(View view) {
         this.view = view;
-        
+
         view.startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 System.out.println("You clicked the start button");
                 view.startQuiz();
+                startQuiz();
+            }
+        });
+    }
+
+    public void startQuiz() {
+        view.optionA.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("You clicked option A");
+            }
+        });
+
+        view.optionB.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("You clicked option B");
+            }
+        });
+
+        view.optionC.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("You clicked option C");
+            }
+        });
+
+        view.optionD.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("You clicked option D");
+            }
+        });
+
+        view.quitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                view.quitScreen();
             }
         });
     }
--- Comp603Assignment2/src/View.java
+++ Comp603Assignment2/src/View.java
@@ -1,9 +1,12 @@
 
+import java.awt.BorderLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 public class View extends JFrame {
 
@@ -11,12 +14,15 @@ public class View extends JFrame {
     JPanel buttonPanel = new JPanel();
     JPanel questionPanel = new JPanel();
     JPanel userPanel = new JPanel();
+    JPanel loserPanel = new JPanel();
+    JPanel quitPanel = new JPanel();
 
-    JLabel welcomeLabel = new JLabel("Welcome to the game");
-    JLabel descriptionLabel = new JLabel("Click button to start the game");
+    JLabel titleLabel = new JLabel("Who wants to be a millionaire?");
     JLabel question = new JLabel("Question");
+    JLabel loserMessage = new JLabel("You lose");
+    JLabel quitMessage = new JLabel("You quit");
     
-    JButton startButton = new JButton("start game");
+    JButton startButton = new JButton("Click button to start the quiz");
 
     JButton optionA = new JButton("A");
     JButton optionB = new JButton("B");
@@ -25,15 +31,23 @@ public class View extends JFrame {
     
     JButton quitButton = new JButton("quit game");
     JButton answerButton = new JButton("answer");
+    
+    JButton resetButton = new JButton("Restart");
 
     public View() {
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(800, 400);
         this.setLocationRelativeTo(null);
-
-        startPanel.add(welcomeLabel);
-        startPanel.add(descriptionLabel);
-        startPanel.add(startButton);
+        
+        startPanel.setLayout(new BorderLayout());
+        
+        titleLabel.setFont(new Font("SansSerif", Font.PLAIN, 50));
+        startButton.setFont(new Font("SansSerif", Font.PLAIN, 20));
+        
+        titleLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        startPanel.add(titleLabel, BorderLayout.CENTER);
+        startPanel.add(startButton, BorderLayout.SOUTH);
 
         this.add(startPanel);
     }
@@ -62,6 +76,36 @@ public class View extends JFrame {
         this.add(questionPanel);
         this.add(buttonPanel);
         this.add(userPanel);
+        this.revalidate();
+        this.repaint();
+    }
+    
+    public void loserScreen() {
+        loserPanel.setFont(new Font("SansSerif", Font.PLAIN, 60));
+        loserPanel.add(loserMessage);
+        
+        this.getContentPane().removeAll();
+        loserPanel.setVisible(true);
+        this.add(loserPanel);
+        this.revalidate();
+        this.repaint();
+    }
+    
+    public void quitScreen(){
+        quitPanel.setLayout(new BorderLayout());
+        
+        quitMessage.setFont(new Font("SansSerif", Font.PLAIN, 50));
+        resetButton.setFont(new Font("SansSerif", Font.PLAIN, 20));
+        
+        quitMessage.setHorizontalAlignment(SwingConstants.CENTER);
+        
+        quitPanel.add(quitMessage, BorderLayout.NORTH);
+        quitPanel.add(resetButton, BorderLayout.SOUTH);
+        
+        
+        this.getContentPane().removeAll();
+        quitPanel.setVisible(true);
+        this.add(quitPanel);
         this.revalidate();
         this.repaint();
     }
